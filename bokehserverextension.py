6d755c56c85165aa3bc8ac699b3c465da2904ccf
rmsare <robertmsare@gmail.com>
2019-03-05 14:19:41 -0800

    Fix whitespace in extension :(

--- bokehserverextension.py
+++ bokehserverextension.py
@@ -2,5 +2,5 @@ from subprocess import Popen
 
 
 def load_jupyter_server_extension(nbapp):
-        """serve the bokeh-app directory with bokeh server"""
-            Popen(["bokeh", "serve", "bokeh-app", "--allow-websocket-origin=*"])
+    """serve the bokeh-app directory with bokeh server"""
+    Popen(["bokeh", "serve", "bokeh-app", "--allow-websocket-origin=*"])
